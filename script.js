f48bbfefcfc867627794ef79be39f0f81766f172
Anamika Yadav <aanyayadav419@gmail.com>
2022-11-28 17:54:15 +0530

    few changes

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
+const board = document.querySelector('#board')
 
 const handler = (event) => {
     const action = event.target.id;
-    if(action === 'rock') {
-        console.log("Rock crushes scissors")
-    } else if(action === 'paper') {
-        console.log("Paper traps Rock")
-    } else if(action === 'scissors') {
-        console.log("Scissors cuts Paper")
-    } else {
-        console.log("Click something")
+
+    switch(action) {
+        case "rock" :
+            board.innerHTML = "Rock crushes scissors";
+            break;
+        case "paper" :
+            board.innerHTML = "Paper traps Rock";
+            break;
+        case "scissors" :
+            board.innerHTML = "Scissors cuts Paper";
+            break;
+        default :
+            board.innerHTML = "Click something";
     }
 }
 
